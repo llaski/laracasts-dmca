8644c18282b904339732e3b10575e36c6c16b96d
Larry Laski <larry.laski@gmail.com>
2015-03-27 19:13:59 -0400

    lesson 8

--- app/Http/Controllers/NoticesController.php
+++ app/Http/Controllers/NoticesController.php
@@ -4,6 +4,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PrepareNoticeRequest;
 use Illuminate\Http\Request;
+use Illuminate\Auth\Guard;
 use App\Provider;
 
 class NoticesController extends Controller {
@@ -39,9 +40,44 @@ class NoticesController extends Controller {
 		return view('notices.create', compact('providers'));
 	}
 
-	public function confirm(PrepareNoticeRequest $request)
+	/**
+	 * Ask user to confirm DMCA to be delivered
+	 *
+	 * @param  PrepareNoticeRequest $request
+	 * @param  Guard                $auth
+	 * @return \Response
+	 */
+	public function confirm(PrepareNoticeRequest $request, Guard $auth)
 	{
-		return $request->all();
+		$template = $this->compileDmcaTemplate($data = $request->all(), $auth);
+
+		session()->flash('dmca', $data);
+
+		return view('notices.confirm', compact('template'));
+	}
+
+	/**
+	 * Compile the DMCA Template from the form data
+	 *
+	 * @param  array $data
+	 * @param  Guard  $auth
+	 * @return mixed
+	 */
+	private function compileDmcaTemplate($data, Guard $auth)
+	{
+		$data = $data + [
+			'name' => $auth->user()->name,
+			'email' => $auth->user()->email
+		];
+
+		return view()->file(app_path('Http/Templates/dmca.blade.php'), $data);
+	}
+
+	public function store()
+	{
+		$data = session()->get('dmca');
+
+		return $data;
 	}
 
 }
